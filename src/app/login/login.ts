a2dd9ea0252dbb4a2139e7f03ccb1f7213fd0996
Lancercrack25 <lancercrack255@gmail.com>
2026-02-25 16:17:35 -0600

    design components implemented

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Registro } from '../registro/registro';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
