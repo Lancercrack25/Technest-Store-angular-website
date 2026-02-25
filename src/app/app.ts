35d33c1b8c2444cc204a6dcf69a9c1920a3f945c
Lancercrack25 <lancercrack255@gmail.com>
2026-02-24 18:54:55 -0600

    global styles on components added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Technest_store_pro');
+
+  constructor(public router: Router) {}
 }
