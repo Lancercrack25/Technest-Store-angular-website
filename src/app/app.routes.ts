5191d3515ebc0187d96af53d0a3484d84eb23ca3
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-03-01 23:00:00 -0600

    Se agrego a las rutas el panel de administrador.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Inicio } from './inicio/inicio';
 import { Login } from './login/login';
 import { Registro } from './registro/registro';
 import { AdminLogin } from './admin-login/admin-login';
+import { AdminPanel } from './admin-panel/admin-panel';
 
 export const routes: Routes = [
-    { path: '', component: Login },
-    { path: 'login', component: Login },
-    { path: 'registro', component: Registro },
-    { path: 'inicio', component: Inicio },
-    { path: 'admin/login', component: AdminLogin},
+  { path: '', component: Login },
+  { path: 'login', component: Login },
+  { path: 'registro', component: Registro },
+  { path: 'inicio', component: Inicio },
+  { path: 'admin/login', component: AdminLogin },
+  { path: 'admin/panel', component: AdminPanel },
 ];
