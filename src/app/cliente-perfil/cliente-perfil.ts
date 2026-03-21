eb9ca37b661b722975ba72ddd0a8df8967def391
Lancercrack25 <lancercrack255@gmail.com>
2026-03-20 19:49:25 -0600

    perfiles cliente y admin creados, aun no diseño

---
+++ src/app/cliente-perfil/cliente-perfil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './cliente-perfil.html',
+  styleUrl: './cliente-perfil.css',
+})
+export class ClientePerfil {
+
+}
