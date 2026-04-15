721845062f24a8e5b66a82ac2bdcb9fc2dfcc32f
Lancercrack25 <lancercrack255@gmail.com>
2026-04-15 17:49:56 -0600

    clientes and provedores for admin panel created, no design yet

---
+++ src/app/provedores/provedores.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provedores',
+  standalone: true,
+  imports: [],
+  templateUrl: './provedores.html',
+  styleUrl: './provedores.css',
+})
+export class Provedores {
+
+}
