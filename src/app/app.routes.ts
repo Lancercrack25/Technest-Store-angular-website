b675eab4155092742170cbd9520e0f3e2b07d1f8
Lancercrack25 <lancercrack255@gmail.com>
2026-05-08 20:58:52 -0600

    componentes eliminados para finalizar mas pronto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,18 +16,6 @@ import { Pedidos } from './pedidos/pedidos';
 import { Ventas } from './ventas/ventas';
 import { RegistroProductos } from './registro-productos/registro-productos';
 import { RegistroProvedores } from './registro-provedores/registro-provedores';
-import { Audifonos } from './audifonos/audifonos';
-import { Cables } from './cables/cables';
-import { Discos } from './discos/discos';
-import { Disipadores } from './disipadores/disipadores';
-import { Gabinetes } from './gabinetes/gabinetes';
-import { Graficas} from './graficas/graficas';
-import { Monitores } from './monitores/monitores';
-import { Motherboards } from './motherboards/motherboards';
-import { Mouses } from './mouses/mouses';
-import { Procesadores } from './procesadores/procesadores';
-import { Rams } from './rams/rams';
-import { Teclados } from './teclados/teclados';
 import { Clientes } from './clientes/clientes';
 import { Provedores } from './provedores/provedores';
 import { BarraBusqueda } from './barra-busqueda/barra-busqueda';
@@ -55,18 +43,6 @@ export const routes: Routes = [
   { path: 'ventas', component: Ventas},
   { path: 'registro/productos', component: RegistroProductos},
   { path: 'registro/provedores', component: RegistroProvedores},
-  { path: 'audifonos', component: Audifonos},
-  { path: 'cables', component: Cables},
-  { path: 'discos', component: Discos},
-  { path: 'disipadores', component: Disipadores},
-  { path: 'gabinetes', component: Gabinetes},
-  { path: 'graficas', component: Graficas},
-  { path: 'monitores', component: Monitores},
-  { path: 'motherboards', component: Motherboards},
-  { path: 'mouses', component: Mouses},
-  { path: 'procesadores', component: Procesadores},
-  { path: 'rams', component: Rams},
-  { path: 'teclados', component: Teclados},
   { path: 'clientes', component: Clientes},
   { path: 'provedores', component: Provedores},
   { path: 'barra-busqueda', component: BarraBusqueda},
