d6115ec7aeb8343d665ded9b751e2fca926d509d
Nika <veronica.calatayud4313@alumnos.udg.mx>
2026-05-10 20:32:43 -0600

    Muestra de todos los productos

--- src/app/productos/productos.ts
+++ src/app/productos/productos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-productos',
@@ -9,5 +10,32 @@ import { Navbar } from "../navbar/navbar";
   styleUrl: './productos.css',
 })
 export class Productos {
+  
+  productos: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.obtenerProductos();
+}
+
+  obtenerProductos() {
+
+  this.http.get<any[]>('http://localhost:3000/productos')
+    .subscribe({
+
+      next: (data) => {
+        this.productos = data;
+        console.log(data);
+      },
+
+      error: (error) => {
+        console.error(error);
+      }
+
+    });
+
+}
+
 
 }
