d45195c1d148bae76a14dad84372aa02864ffd96
Nika <veronica.calatayud4313@alumnos.udg.mx>
2026-05-12 20:16:13 -0600

    boton agregar al carrito funcional

--- src/app/productos/productos.ts
+++ src/app/productos/productos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
 import { HttpClient } from '@angular/common/http';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-productos',
@@ -34,6 +35,68 @@ export class Productos {
       }
 
     });
+
+}
+
+agregarAlCarrito(producto: any) {
+
+  // obtener cliente guardado
+  const cliente = JSON.parse(localStorage.getItem('cliente') || '{}');
+
+  // validar login
+  if (!cliente.id_cliente) {
+
+    Swal.fire({
+      icon: 'warning',
+      title: 'Inicia sesión',
+      text: 'Debes iniciar sesión para agregar productos'
+    });
+
+    return;
+  }
+
+  // crear carrito
+  this.http.post<any>('http://localhost:3000/carrito', {
+    id_cliente: cliente.id_cliente
+  }).subscribe({
+
+    next: (carrito) => {
+
+      // agregar producto al carrito
+      this.http.post('http://localhost:3000/carrito/detalle', {
+
+        id_carrito: carrito.id_carrito,
+        id_producto: producto.id_producto,
+        cantidad: 1
+
+      }).subscribe({
+
+        next: () => {
+
+          Swal.fire({
+            icon: 'success',
+            title: 'Producto agregado',
+            timer: 1200,
+            showConfirmButton: false
+          });
+
+        },
+
+        error: () => {
+
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'No se pudo agregar al carrito'
+          });
+
+        }
+
+      });
+
+    }
+
+  });
 
 }
 
