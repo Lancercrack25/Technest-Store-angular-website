f2986cc2dac8fec863107e0aa67e5b406febc3a5
Lancercrack25 <lancercrack255@gmail.com>
2026-05-15 17:51:38 -0600

    new changes added to the proyect

--- src/app/forgot-password/forgot-password.ts
+++ src/app/forgot-password/forgot-password.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css',
 })
 export class ForgotPassword {
+  email: string = '';
 
+  enviarRecuperacion(): void {
+
+    const correo = this.email.trim();
+
+    // Validar campo vacío
+    if (!correo) {
+      this.mostrarAlerta(
+        'warning',
+        'Correo no ingresado',
+        'Debe ingresar un correo para recuperar su contraseña.'
+      );
+      return;
+    }
+
+    // Validar formato de correo
+    if (!this.validarCorreo(correo)) {
+      this.mostrarAlerta(
+        'error',
+        'Correo inválido',
+        'Ingrese un correo electrónico válido.'
+      );
+      return;
+    }
+
+    // Simulación de envío
+    this.mostrarAlerta(
+      'success',
+      'Correo enviado',
+      'En unos momentos recibirá un enlace para recuperar su contraseña.'
+    );
+
+    // Limpiar input
+    this.email = '';
+  }
+
+  validarCorreo(correo: string): boolean {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(correo);
+  }
+
+  mostrarAlerta(
+    icono: 'success' | 'error' | 'warning',
+    titulo: string,
+    mensaje: string
+  ): void {
+    Swal.fire({
+      icon: icono,
+      title: titulo,
+      text: mensaje,
+      confirmButtonColor: '#3085d6',
+    });
+  }
 }
